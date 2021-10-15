d076a3ee7b596160dece6ebf6a8b358c91530b3c
bishal-kumar-shaw <bishal.kumar.shaw@valuence.inc>
2021-10-15 09:46:54 +0530

    Added some necessary files with minor code update

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -6,22 +6,22 @@ import pandas as pd
 
 class PreProcessing():
     def __init__(self, csv_dir) -> None:
-        dict_path = "../label_dict.json"
+        dict_path = "../config/label_dict.json"
         if os.path.isfile(dict_path):
             with open(dict_path) as f:
                 self.label_dict = json.load(f)
         else:
-            df = pd.read_csv(os.path.join(csv_dir, "train.csv"))
+            df = pd.read_csv(os.path.join(csv_dir, "train_data.csv"))
             self.label_dict = self.create_dict(df)
         
-        self.train_df = self.create_encoded_df(os.path.join(csv_dir, "train.csv"))
-        self.test_df = self.create_encoded_df(os.path.join(csv_dir, "test.csv"))
+        self.train_df = self.create_encoded_df(os.path.join(csv_dir, "train_data.csv"))
+        self.test_df = self.create_encoded_df(os.path.join(csv_dir, "test_data.csv"))
 
     def create_dict(self, df):
         label_unique = np.unique(df.Genus)
         label_dict = {label: i for i, label in enumerate(label_unique)}
 
-        with open("../label_dict.json", 'w') as f:
+        with open("../config/label_dict.json", 'w') as f:
             json.dump(label_dict, f, indent=4)
         
         return label_dict
